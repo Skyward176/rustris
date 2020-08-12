325afd181ee530948188602f8abfb70a027e885b
Brandon Martinez <brandonam04@outlook.com>
2020-08-12 09:03:21 -0400

    Begun implementation of piece spawningfunctions, separated board drawing into separate functions. Introduced bug in spawning function where piece is not completely drawn.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 fn main() {
-    let game = board_constructor(10,20);
+    let mut game = board_constructor(10,20);
+    //draw_board(&game);
+    game = spawn_i_block(game);
+    draw_board(&game);
+}
+fn draw_board(game: &Board) {
     for i in 0..game.height {
         for x in 0..game.width {
             print!("{}", game.board[i][x]);
@@ -19,4 +24,14 @@ struct Board {
     board: Vec<Vec<String>>,
     width: usize,
     height: usize,    
+}
+fn spawn_i_block(mut board: Board) -> Board{
+    let pos_x = 3;
+    let pos_y = 0;
+    let shape = [0; 4];
+    println!("{:?}", shape);
+    for i in 0..shape.len() {
+        board.board[pos_y][pos_x] = shape[i].to_string();
+    }
+    board
 }
